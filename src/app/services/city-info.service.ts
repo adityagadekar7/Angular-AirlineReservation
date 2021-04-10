5dd968a8942e2214def6788b846ecbb24d1ee12d
FALGUNI TAILOR <FALGUNI TAILOR@LAPTOP-5D94CUVJ>
2021-04-10 12:20:19 +0530

    no change

--- src/app/services/city-info.service.ts
+++ src/app/services/city-info.service.ts
@@ -12,8 +12,8 @@ import{HttpHeaders} from '@angular/common/http';
 export class CityInfoService {
   city:CityInfoModule;
   http:HttpClient;
-  url:string='http://localhost:62227/api/City_Information';
-  //url:string='http://localhost:56797/api/City_Information';
+  //url:string='http://localhost:62227/api/City_Information';
+  url:string='http://localhost:56797/api/City_Information';
 
   httpOptions = {headers: new HttpHeaders({
     'Content-Type': 'application/json'
