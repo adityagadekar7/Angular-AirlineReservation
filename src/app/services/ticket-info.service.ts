8cb586cbe55d5dba30e6a21bd51b13cee09a5f90
adityagadekar7 <adityagadekar07@gmail.com>
2021-04-13 00:13:16 +0530

    latest

--- src/app/services/ticket-info.service.ts
+++ src/app/services/ticket-info.service.ts
@@ -6,6 +6,7 @@ import{Observer} from 'rxjs';
 import{Observable} from 'rxjs/internal/Observable';
 import{HttpHeaders} from '@angular/common/http';
 import { TicketInfoModule } from '../modules/ticket-info/ticket-info.module';
+import { CancellationInfoModule } from '../modules/cancellation-info/cancellation-info.module';
 
 
 @Injectable({
@@ -15,8 +16,8 @@ export class TicketInfoService {
   //tick:TicketInfoModule;
   http:HttpClient;
   //url:string='http://localhost:62227/api/Dashboard'
-  //url:string='http://localhost:59875/api/Dashboard'
-  url:string='http://localhost:56797/api/Dashboard'
+  url:string='http://localhost:59875/api/Dashboard'
+  //url:string='http://localhost:56797/api/Dashboard'
   httpOptions = {headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
@@ -30,5 +31,27 @@ export class TicketInfoService {
   GetCancelledTickets(id:number):Observable<TicketInfoModule[]>{
     return this.http.get<TicketInfoModule[]>(this.url+'/GetCancelledTickets/'+id);
   }
+
+  ////--------------------------------------------------------------------------
+  //Get By ID - Insert into Cancellation - Delete From Booked
+  //Get By ID
+  GetBookedTicketByPnr(pnr:number):Observable<TicketInfoModule>{
+    return this.http.get<TicketInfoModule>(this.url+'/GetBookedTicketByPnr/'+pnr);
+  }
+  //Insert into Cancellation
+  CancelInsertTicket(ct:CancellationInfoModule):Observable<boolean>
+  {
+    return this.http.post<boolean>(this.url+'/InsertInCancelled',ct,this.httpOptions);
+  }
+
+  UpdateBookedTickets(pnr:number,ti:TicketInfoModule):Observable<boolean>
+{
+  return this.http.put<boolean>(this.url+'/'+'UpdateBookedTickets'+'/'+ pnr,ti,this.httpOptions); //passing 2 parameters
+}
+
+  //DeleteFromBooked
+  // CancelDeleteTicket(pnr:number):Observable<boolean>{
+  //   return this.http.delete<boolean>(this.url+'/DeleteFromBooked/'+pnr);
+  // }
 
 }
